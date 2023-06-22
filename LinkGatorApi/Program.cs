52dc07c8f350dde882ccfefb1c3afd203a4c44f5
John Singh <wethegreenpeople@gmail.com>
2023-06-22 12:29:25 -0700

    Configure app for docker deployment

--- LinkGatorApi/Program.cs
+++ LinkGatorApi/Program.cs
@@ -66,4 +66,29 @@ app.MapGraphQL();
 
 app.MapGet("/", () => "Hello World!");
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    if (context.Database.GetPendingMigrations().Any())
+    {
+        context.Database.Migrate();
+    }
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    // In non-development environments, bind to the correct port
+    var port = Environment.GetEnvironmentVariable("PORT");
+    if (!string.IsNullOrEmpty(port))
+    {
+        builder.WebHost.UseUrls($"http://*:{port}");
+    }
+}
+
 app.Run();
