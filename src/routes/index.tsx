d7cfa21df8f5a61f7a88e1cbebeea2245af8d34d
John Singh <wethegreenpeople@gmail.com>
2025-05-08 13:24:10 -0700

    Add follower retrieval and display in Home component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,32 @@
-import { A } from "@solidjs/router";
+import { A, createAsync, query } from "@solidjs/router";
+import { For } from "solid-js";
 import Counter from "~/components/Counter";
+import { kv } from "~/middleware";
+
+const getFollowers = query(async () => {
+  "use server"
+
+  const followers: string[] = [];
+  for await (const item of kv.list<string>({prefix: ["followers"]})) {
+    if (followers.includes(item.value)) continue;
+    followers.push(item.value);
+  }
+  return followers;
+}, "getFollowers");
+
+export const route = {
+  preload: () => getFollowers()
+}
 
 export default function Home() {
+  const followers = createAsync(() => getFollowers());
+
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Hello world!</h1>
+      <ul class="text-white">
+        <For each={followers()}>{(follower) => <li>{follower}</li>}</For>
+      </ul>
       <Counter />
       <p class="mt-8">
         Visit{" "}
